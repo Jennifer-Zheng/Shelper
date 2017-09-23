3f8938e768f7cdb0c67d64f9c7930f44e2263dcd
Kevin Kang <kevink97@cs.washington.edu>
2017-09-23 16:35:11 -0500

    working rest api

--- server/application.py
+++ server/application.py
@@ -179,4 +179,4 @@ def add_product():
 
 ###################EXAMPLE CODE BELOW############################
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
