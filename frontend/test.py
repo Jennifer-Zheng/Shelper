c0e83708bb23fba4075a94b82b09128a586d6185
Zhe Yu <Alan@Zhes-MacBook.local>
2017-09-24 09:56:38 -0500

    add customized 'Go To Next Page' link

--- frontend/test.py
+++ frontend/test.py
@@ -6,7 +6,7 @@ client = MongoClient('mongodb://user:pass@ds149134.mlab.com:49134/hackrice17')
 db = client['hackrice17']
 
 
-url = 'http://0.0.0.0:8080/products'
+url = 'http://0.0.0.0:8080/shelters'
 result = requests.get(url)
 json= result.json()
 
