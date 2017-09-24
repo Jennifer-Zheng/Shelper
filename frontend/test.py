b973f50dbbc560c546c9f7ec0ce056ef1e34f75b
Zhe Yu <Alan@Zhes-MacBook.local>
2017-09-24 09:22:34 -0500

    new googleapi.html

--- frontend/test.py
+++ frontend/test.py
@@ -5,8 +5,10 @@ import os
 client = MongoClient('mongodb://user:pass@ds149134.mlab.com:49134/hackrice17')
 db = client['hackrice17']
 
-url = 'http://0.0.0.0:8080/shelters'
+
+url = 'http://0.0.0.0:8080/products'
 result = requests.get(url)
 json= result.json()
 
 print (json)
+
